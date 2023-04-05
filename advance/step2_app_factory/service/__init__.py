5a7c63ff343765bc8c88072ff681bd40631c5a80
hoonaianooh <hoonaianooh@gmail.com>
2023-04-05 13:28:02 +0900

    feat: 질문 관련 등록,수정,조회,삭제 ORM 함수, 답변등록, 상호 참조, 질문 삭제시 답변 상황 체크

--- advance/step2_app_factory/service/__init__.py
+++ advance/step2_app_factory/service/__init__.py
@@ -25,11 +25,11 @@ def create_app():
     return app
 
 def init_database( app ):
-    # pool
-    from .model import pool_sql
-    pool_sql.init_pool()
-    # 테스트
-    print( pool_sql.login('guest', '1234') )
+    # # pool
+    # from .model import pool_sql
+    # pool_sql.init_pool()
+    # # 테스트
+    # print( pool_sql.login('guest', '1234') )
 
     # ORM을 위한 flask객체와, sql 객체, migrate 객체 연결
     db.init_app( app )
